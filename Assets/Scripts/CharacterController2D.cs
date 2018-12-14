61123db6d9a6af6c3da3fba874233809ace77c16
LW\LW <ladimitr01@gmail.com>
2018-12-14 13:27:15 +0200

    Initial commit

---
+++ Assets/Scripts/CharacterController2D.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController2D : CharacterStats
+{
+    private void Awake()
+    {
+
+    }
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        Movement(MovementSpeed);
+        FlipArt();
+    }
+
+
+    private void Movement(float speed)
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+
+        rb.velocity = new Vector2(x, y).normalized * speed;
+    }
+
+    private void FlipArt()
+    {
+        if (rb.velocity.x > 0 && artParent.localScale.x > 0 )
+            artParent.localScale = new Vector3(-1, 1, 1);
+        else if (rb.velocity.x < 0 && artParent.localScale.x < 0)
+            artParent.localScale = Vector3.one;
+    }
+}
---
+++ Assets/Scripts/CharacterStats.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+
+public class CharacterStats : MonoBehaviour
+{
+    [Header("Player's Stats")]
+
+    [SerializeField]
+    [Tooltip("Player's speed")]
+    protected float MovementSpeed = 10f;
+
+    [Header("Required Components")]
+
+    [SerializeField]
+    [Tooltip("Player's Rigidbody2D")]
+    protected Rigidbody2D rb;
+
+    [SerializeField]
+    [Tooltip("The player's gameobject that contains the art")]
+    protected Transform artParent;
+}

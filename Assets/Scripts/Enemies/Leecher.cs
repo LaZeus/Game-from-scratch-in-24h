759dc3d20b6766b03da0195b7b5b3f03a41906d1
LW\LW <ladimitr01@gmail.com>
2018-12-14 18:11:48 +0200

    leech added

---
+++ Assets/Scripts/Enemies/Leecher.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Leecher : MonoBehaviour
+{ 
+    [SerializeField]
+    private Animator anim;
+
+    [SerializeField]
+    private float movementSpeed;
+
+    [SerializeField]
+    private Transform points;
+
+    [SerializeField]
+    private int nextPoint = 0;
+
+    private void Start()
+    {
+        if (points == null)
+            points = transform.parent.Find("Points");
+    }
+
+    private void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, points.GetChild(nextPoint).position, movementSpeed * Time.deltaTime);
+
+        if (transform.position == points.GetChild(nextPoint).position)
+        {
+            nextPoint++;
+            if (nextPoint >= points.childCount)
+                nextPoint = 0;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            col.transform.Find("MCHead");
+        }
+    }
+}

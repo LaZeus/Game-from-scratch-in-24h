68b028db18789e9cd39e4a3f2ba6dadf9572fda3
LW\LW <ladimitr01@gmail.com>
2018-12-14 17:29:23 +0200

    Enemy now perform attacks

--- Assets/Scripts/Enemies/Turret.cs
+++ Assets/Scripts/Enemies/Turret.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class Turret : MonoBehaviour
 {
+
+    [SerializeField]
+    private Animator anim;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("Attack", 1, 3);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void Attack()
+    {
+        anim.SetTrigger("Attack");
     }
 }
